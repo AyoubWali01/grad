54f402240532cb3a56b791d8fa3862033e90298c
Brahem <131829061+BrahemMubark@users.noreply.github.com>
2024-12-17 19:43:43 +0200

    .

--- database/migrations/2024_12_17_172606_create_orders_table.php
+++ database/migrations/2024_12_17_172606_create_orders_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade'); // مفتاح خارجي
+            $table->foreignId('order_status_id')->constrained('order_statuses')->onDelete('cascade'); // مفتاح خارجي
+            $table->enum('payment_method', ['cash', 'credit_card', 'paypal']); // تحديد قيم الدفع
+            $table->decimal('total_price', 10, 2);
             $table->timestamps();
         });
     }
